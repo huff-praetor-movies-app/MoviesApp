88562885c9045a71c6e9735a272d4ab0ced7ee09
Byron Praetor <byronpc18@icloud.com>
2023-12-06 14:51:32 -0600

    create,delete and update added(kinda)

--- js/movies-application.js
+++ js/movies-application.js
@@ -20,5 +20,66 @@ const getMovies = async () => {
         console.error(e)
     }
 }
-
 getMovies()
+
+const createMovie = async (movie) =>{
+    try{
+        const url = 'http://localhost:3000/movies';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(movie)
+        };
+        const response = await fetch(url,options);
+        const newMovie = await response.json();
+        return newMovie;
+    }catch(e){
+        console.error(e)
+    }
+}
+//Create new movie
+// (async () => {
+//     const newMovie = await createMovie({
+//     title: 'Karate Kid',
+//     rating: '5',
+//     genre: 'action'
+// });
+// console.log(newMovie)
+// })();
+
+//Update movie
+// const updateMovie = async (id, movie) =>{
+//     try{
+//         const url = 'http://localhost:3000/movies';
+//         const options = {
+//             method: 'PATCH',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(movie)
+//         };
+//         const response = await fetch(url,options);
+//         const updatedMovie = await response.json();
+//         return updatedMovie;
+//     }catch(e){
+//         console.error(e)
+//     }
+// }
+
+//Delete movie
+// const deleteMovie = async (id)=>{
+//     try{
+//         const url = 'http://localhost:3000/movies';
+//         const options = {
+//             method: 'DELETE'
+//         };
+//         const response = await fetch(url,options);
+//         const deletedMovie = await response.json();
+//         return deletedMovie;
+//     }catch (e) {
+//
+//     }
+// }
+
