5b507b5df0f23ac19b923439414b6b6d9110f17a
Ira Huff <ira.m.huff@gmail.com>
2023-12-08 09:40:11 -0600

    working on poster

--- js/movies-application.js
+++ js/movies-application.js
@@ -1,55 +1,59 @@
-import { deleteMovie, updateMovie, getMovie, createMovie, getMovies } from "./moviesAPI.js";
+import {deleteMovie, updateMovie, getMovie, createMovie, getMovies, getPoster} from "./moviesAPI.js";
+
 const insert = document.querySelector('.cards')
+const load = document.querySelector('#loading')
+let id
 let id2
+
+let poster = getPoster('shrek 3')
+console.log(poster);
 
 function handleSearch() {
     const searchInput = document.querySelector('.form-control');
     const movieName = searchInput.value.toLowerCase();
     insert.innerHTML = ''
     load.showModal()
-    getMovies().then(movies =>{
-        for (let movie of movies){
-            if(movie.title.toLowerCase().includes(movieName)){
+    getMovies().then(movies => {
+        for (let movie of movies) {
+            if (movie.title.toLowerCase().includes(movieName)) {
                 id = movie.id;
                 showSearchedMovie(id);
-            }
-            else if(movie.genre.toLowerCase().includes(movieName)){
+            } else if (movie.genre.toLowerCase().includes(movieName)) {
                 id = movie.id;
                 showSearchedMovie(id);
-            }
-            else{
-                load.close();
             }
         }
-    })
+    });
+    load.close();
 }
-function showSearchedMovie(id){
+
+function showSearchedMovie(id) {
     drawMovie(id)
 }
 
-const searchMovies = () =>{
+const searchMovies = () => {
     const submitButton = document.querySelector('#submit');
-    submitButton.addEventListener("click", (e)=>{
+    submitButton.addEventListener("click", (e) => {
         e.preventDefault();
         handleSearch()
     })
 }
 searchMovies()
 
-const addNewMovie = () =>{
+const addNewMovie = () => {
     const addImage = document.querySelector('#addMovieImage');
-    addImage.addEventListener("click", (e)=>{
+    addImage.addEventListener("click", (e) => {
         e.preventDefault()
         document.querySelector('#create').showModal();
     })
 }
 addNewMovie();
 
-const del = async(id) => {
+const del = async (id) => {
     try {
         await deleteMovie(id)
         populateMovies()
-    }catch (e) {
+    } catch (e) {
         console.error(e)
     }
 }
@@ -58,7 +62,7 @@ const update = async (id, movie) => {
     try {
         await updateMovie(id, movie)
         populateMovies()
-    }catch(e) {
+    } catch (e) {
         console.error(e)
     }
 }
@@ -66,12 +70,10 @@ const create = async (movie) => {
     try {
         await createMovie(movie)
         populateMovies()
-    }catch(e) {
+    } catch (e) {
         console.error(e)
     }
 }
-const load = document.querySelector('#loading')
-let id
 const populateMovies = () => {
     load.showModal()
     getMovies().then(movies => {
@@ -84,41 +86,42 @@ const populateMovies = () => {
 
 populateMovies()
 
-function updateForm(id)  {
+function updateForm(id) {
     load.showModal()
     let formTitle = document.querySelector('#update-title');
     let formRating = document.querySelector('#update-rating');
     let formGenre = document.querySelector('#update-genre');
     fetch("http://localhost:3000/movies/" + id).then(resp => resp.json()).then(movie => {
-       formTitle.value = movie.title;
+        formTitle.value = movie.title;
         formRating.value = movie.rating;
         formGenre.value = movie.genre;
         document.querySelector('#update').showModal();
         id2 = id
         load.close()
     })
-
 }
+
 document.querySelector('#cancelUpdate').addEventListener('click', () => {
     document.querySelector('#update').close()
 })
 
 //need to convert into a function --ira
 document.querySelector('#submitUpdate').addEventListener("click", (event) => {
-    event.preventDefault();
-    document.querySelector('#update').close()
-    let title = document.querySelector('#update-title').value;
-    let rating = document.querySelector('#update-rating').value;
-    if (rating >=5) {
-        rating = 5
-    }else if (rating <=0) {
-        rating = 0
-    }else if (isNaN(rating)){
-        rating = 0
-    }
-    let genre = document.querySelector('#update-genre').value;
+        event.preventDefault();
+        document.querySelector('#update').close()
+        let title = document.querySelector('#update-title').value;
+        let rating = document.querySelector('#update-rating').value;
+        if (rating >= 5) {
+            rating = 5
+        } else if (rating <= 0) {
+            rating = 0
+        } else if (isNaN(rating)) {
+            rating = 0
+        }
+        let genre = document.querySelector('#update-genre').value;
+
     console.log(id)
-    update(id2, {title, rating, genre})
+        update(id2, {title, rating, genre, poster})
     }
 )
 document.querySelector('#createNewMovie').addEventListener("click", (event) => {
@@ -126,62 +129,59 @@ document.querySelector('#createNewMovie').addEventListener("click", (event) => {
         document.querySelector('#create').close()
         let title = document.querySelector('#create-title').value;
         let rating = parseFloat(document.querySelector('#create-rating').value);
-        if (rating >=5) {
+        if (rating >= 5) {
             rating = 5
-        }else if (rating <=0) {
+        } else if (rating <= 0) {
             rating = 0
-        }else if (isNaN(rating)){
+        } else if (isNaN(rating)) {
             rating = 0
         }
         let genre = document.querySelector('#create-genre').value;
         create({title, rating, genre})
-
     }
 )
 
 
-function drawMovie(id) {
-    getMovie(id).then (movie => {
-
-            let div = document.createElement('div');
-            div.classList.add('card');
-            div.innerHTML = `<h3 class=title>${movie.title}</h3>
+async function drawMovie(id)  {
+   await getMovie(id).then(movie => {
+        let div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `<h3 class=title>${movie.title}</h3>
                                 <p>${movie.rating} of 5</p>
                                <p>${movie.genre}</p>`
-            insert.appendChild(div)
+        insert.appendChild(div)
 
-            const image= document.createElement('img');
-            image.setAttribute('src', 'img/icons8-edit-50.png');
-            image.setAttribute(`data-id`, movie.id)
-            image.setAttribute('id', 'edit-image')
-            div.appendChild(image);
+        const image = document.createElement('img');
+        image.setAttribute('src', 'img/icons8-edit-50.png');
+        image.setAttribute(`data-id`, movie.id)
+        image.setAttribute('id', 'edit-image')
+        div.appendChild(image);
 
-            const imageDelete= document.createElement('img');
-            imageDelete.setAttribute('src', 'img/icons8-delete-50.png');
-            imageDelete.setAttribute(`data-id`, movie.id)
-            imageDelete.setAttribute('id', 'delete-image')
-            div.appendChild(imageDelete);
+        const imageDelete = document.createElement('img');
+        imageDelete.setAttribute('src', 'img/icons8-delete-50.png');
+        imageDelete.setAttribute(`data-id`, movie.id)
+        imageDelete.setAttribute('id', 'delete-image')
+        div.appendChild(imageDelete);
 
-            imageDelete.addEventListener("click", evt => {
-                load.showModal()
-                evt.preventDefault()
-                evt.stopPropagation()
-                id = evt.target.dataset.id;
-                console.log(id);
-                del(id)
-            })
+        imageDelete.addEventListener("click", evt => {
+            evt.preventDefault()
+            evt.stopPropagation()
+            id = evt.target.dataset.id;
+            console.log(id);
+            del(id)
+        })
 
-            image.addEventListener("click", evt => {
-                evt.preventDefault()
-                evt.stopPropagation()
-                id = evt.target.dataset.id;
-                console.log(id);
-                updateForm(id)
-            })
-        }); load.close();
-
+        image.addEventListener("click", evt => {
+            evt.preventDefault()
+            evt.stopPropagation()
+            id = evt.target.dataset.id;
+            console.log(id);
+            updateForm(id)
+        })
+    });
+    load.close();
 }
 
-document.querySelector('#cancelCreate').addEventListener('click', () =>{
+document.querySelector('#cancelCreate').addEventListener('click', () => {
     document.querySelector('#create').close()
 })
